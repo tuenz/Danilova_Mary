84c59f89ae797c58a3171b03fef8cf053a5d3770
tuenz <70901283+tuenz@users.noreply.github.com>
2020-09-23 16:55:11 +0300

    project1

---
+++ DanilovaM/DanilovaM/DanilovaM.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Pipeline
+{
+	int diameter;
+	int length;
+};
+
+Pipeline InputPipeline()
+{
+	Pipeline p;
+	cout << "Input diameter:";
+	cin >> p.diameter;
+	cout << "Input length:";
+	cin >> p.length;
+	return p;
+}
+void PrintPipeline(const Pipeline& p)
+{
+	cout << "\nDiameter: " << p.diameter << "\tLength: " << p.length << endl;
+}
+void EditPipeline(Pipeline& p)
+{
+	p.diameter -= 1;
+}
+int main()
+{
+	Pipeline pipe = InputPipeline();
+	EditPipeline(pipe);
+	PrintPipeline(pipe);
+	return 0;
+}
+
+
